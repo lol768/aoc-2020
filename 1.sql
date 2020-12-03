bcf6c88321edc91624b37533d479ee1f0680fbc0
Adam Williams <lol768@users.noreply.github.com>
2020-12-03 01:15:08 +0000

    Add 1.sql

---
+++ 1.sql
@@ -0,0 +1,419 @@
+
+create table day1
+(
+    val int not null
+);
+
+insert into day1 (val)
+values (1348);
+insert into day1 (val)
+values (1621);
+insert into day1 (val)
+values (1500);
+insert into day1 (val)
+values (1818);
+insert into day1 (val)
+values (1266);
+insert into day1 (val)
+values (1449);
+insert into day1 (val)
+values (1880);
+insert into day1 (val)
+values (1416);
+insert into day1 (val)
+values (1862);
+insert into day1 (val)
+values (1665);
+insert into day1 (val)
+values (1588);
+insert into day1 (val)
+values (1704);
+insert into day1 (val)
+values (1922);
+insert into day1 (val)
+values (1482);
+insert into day1 (val)
+values (1679);
+insert into day1 (val)
+values (1263);
+insert into day1 (val)
+values (1137);
+insert into day1 (val)
+values (1045);
+insert into day1 (val)
+values (1405);
+insert into day1 (val)
+values (1048);
+insert into day1 (val)
+values (1619);
+insert into day1 (val)
+values (1520);
+insert into day1 (val)
+values (455);
+insert into day1 (val)
+values (1142);
+insert into day1 (val)
+values (1415);
+insert into day1 (val)
+values (1554);
+insert into day1 (val)
+values (1690);
+insert into day1 (val)
+values (1886);
+insert into day1 (val)
+values (1891);
+insert into day1 (val)
+values (1701);
+insert into day1 (val)
+values (1915);
+insert into day1 (val)
+values (1521);
+insert into day1 (val)
+values (1253);
+insert into day1 (val)
+values (1580);
+insert into day1 (val)
+values (1376);
+insert into day1 (val)
+values (1564);
+insert into day1 (val)
+values (1747);
+insert into day1 (val)
+values (1814);
+insert into day1 (val)
+values (1749);
+insert into day1 (val)
+values (1485);
+insert into day1 (val)
+values (1969);
+insert into day1 (val)
+values (974);
+insert into day1 (val)
+values (1566);
+insert into day1 (val)
+values (1413);
+insert into day1 (val)
+values (1451);
+insert into day1 (val)
+values (1200);
+insert into day1 (val)
+values (1558);
+insert into day1 (val)
+values (1756);
+insert into day1 (val)
+values (1910);
+insert into day1 (val)
+values (1044);
+insert into day1 (val)
+values (470);
+insert into day1 (val)
+values (1620);
+insert into day1 (val)
+values (1772);
+insert into day1 (val)
+values (1066);
+insert into day1 (val)
+values (1261);
+insert into day1 (val)
+values (1776);
+insert into day1 (val)
+values (988);
+insert into day1 (val)
+values (1976);
+insert into day1 (val)
+values (1834);
+insert into day1 (val)
+values (1896);
+insert into day1 (val)
+values (1646);
+insert into day1 (val)
+values (1626);
+insert into day1 (val)
+values (1300);
+insert into day1 (val)
+values (1692);
+insert into day1 (val)
+values (1204);
+insert into day1 (val)
+values (2006);
+insert into day1 (val)
+values (1265);
+insert into day1 (val)
+values (1911);
+insert into day1 (val)
+values (1361);
+insert into day1 (val)
+values (1766);
+insert into day1 (val)
+values (1750);
+insert into day1 (val)
+values (2000);
+insert into day1 (val)
+values (1824);
+insert into day1 (val)
+values (1726);
+insert into day1 (val)
+values (1672);
+insert into day1 (val)
+values (651);
+insert into day1 (val)
+values (1226);
+insert into day1 (val)
+values (1954);
+insert into day1 (val)
+values (1055);
+insert into day1 (val)
+values (1999);
+insert into day1 (val)
+values (1793);
+insert into day1 (val)
+values (1640);
+insert into day1 (val)
+values (1567);
+insert into day1 (val)
+values (1040);
+insert into day1 (val)
+values (1426);
+insert into day1 (val)
+values (1717);
+insert into day1 (val)
+values (1658);
+insert into day1 (val)
+values (1864);
+insert into day1 (val)
+values (1917);
+insert into day1 (val)
+values (695);
+insert into day1 (val)
+values (1071);
+insert into day1 (val)
+values (1573);
+insert into day1 (val)
+values (1897);
+insert into day1 (val)
+values (1546);
+insert into day1 (val)
+values (1727);
+insert into day1 (val)
+values (1801);
+insert into day1 (val)
+values (1259);
+insert into day1 (val)
+values (1290);
+insert into day1 (val)
+values (1481);
+insert into day1 (val)
+values (1148);
+insert into day1 (val)
+values (1332);
+insert into day1 (val)
+values (1262);
+insert into day1 (val)
+values (1536);
+insert into day1 (val)
+values (1184);
+insert into day1 (val)
+values (1821);
+insert into day1 (val)
+values (1681);
+insert into day1 (val)
+values (1671);
+insert into day1 (val)
+values (1612);
+insert into day1 (val)
+values (1678);
+insert into day1 (val)
+values (1703);
+insert into day1 (val)
+values (1604);
+insert into day1 (val)
+values (1697);
+insert into day1 (val)
+values (2003);
+insert into day1 (val)
+values (1453);
+insert into day1 (val)
+values (1493);
+insert into day1 (val)
+values (1797);
+insert into day1 (val)
+values (1180);
+insert into day1 (val)
+values (1234);
+insert into day1 (val)
+values (1775);
+insert into day1 (val)
+values (1859);
+insert into day1 (val)
+values (1388);
+insert into day1 (val)
+values (1393);
+insert into day1 (val)
+values (667);
+insert into day1 (val)
+values (1767);
+insert into day1 (val)
+values (1429);
+insert into day1 (val)
+values (1990);
+insert into day1 (val)
+values (1322);
+insert into day1 (val)
+values (1684);
+insert into day1 (val)
+values (1696);
+insert into day1 (val)
+values (1565);
+insert into day1 (val)
+values (1380);
+insert into day1 (val)
+values (1745);
+insert into day1 (val)
+values (1685);
+insert into day1 (val)
+values (1189);
+insert into day1 (val)
+values (1396);
+insert into day1 (val)
+values (1593);
+insert into day1 (val)
+values (1850);
+insert into day1 (val)
+values (1722);
+insert into day1 (val)
+values (1495);
+insert into day1 (val)
+values (1844);
+insert into day1 (val)
+values (1285);
+insert into day1 (val)
+values (1483);
+insert into day1 (val)
+values (1635);
+insert into day1 (val)
+values (1072);
+insert into day1 (val)
+values (1947);
+insert into day1 (val)
+values (1109);
+insert into day1 (val)
+values (1586);
+insert into day1 (val)
+values (1730);
+insert into day1 (val)
+values (1723);
+insert into day1 (val)
+values (1246);
+insert into day1 (val)
+values (1389);
+insert into day1 (val)
+values (1135);
+insert into day1 (val)
+values (1827);
+insert into day1 (val)
+values (1531);
+insert into day1 (val)
+values (1583);
+insert into day1 (val)
+values (1743);
+insert into day1 (val)
+values (1958);
+insert into day1 (val)
+values (183);
+insert into day1 (val)
+values (1323);
+insert into day1 (val)
+values (1949);
+insert into day1 (val)
+values (1799);
+insert into day1 (val)
+values (1269);
+insert into day1 (val)
+values (1379);
+insert into day1 (val)
+values (1950);
+insert into day1 (val)
+values (1592);
+insert into day1 (val)
+values (1467);
+insert into day1 (val)
+values (1052);
+insert into day1 (val)
+values (1418);
+insert into day1 (val)
+values (2009);
+insert into day1 (val)
+values (1227);
+insert into day1 (val)
+values (1254);
+insert into day1 (val)
+values (1865);
+insert into day1 (val)
+values (1609);
+insert into day1 (val)
+values (1848);
+insert into day1 (val)
+values (1653);
+insert into day1 (val)
+values (1691);
+insert into day1 (val)
+values (1633);
+insert into day1 (val)
+values (1349);
+insert into day1 (val)
+values (1104);
+insert into day1 (val)
+values (1790);
+insert into day1 (val)
+values (1755);
+insert into day1 (val)
+values (1847);
+insert into day1 (val)
+values (1598);
+insert into day1 (val)
+values (1872);
+insert into day1 (val)
+values (1478);
+insert into day1 (val)
+values (1778);
+insert into day1 (val)
+values (1952);
+insert into day1 (val)
+values (1694);
+insert into day1 (val)
+values (1238);
+insert into day1 (val)
+values (1825);
+insert into day1 (val)
+values (1508);
+insert into day1 (val)
+values (1141);
+insert into day1 (val)
+values (1464);
+insert into day1 (val)
+values (1838);
+insert into day1 (val)
+values (1292);
+insert into day1 (val)
+values (1403);
+insert into day1 (val)
+values (1365);
+insert into day1 (val)
+values (1494);
+insert into day1 (val)
+values (934);
+insert into day1 (val)
+values (1235);
+
+select v1*v2 from (
+select (d1.val + d2.val) as sum, d1.val as v1, d2.val as v2
+from day1 d1
+         cross join day1 d2) where sum = 2020;
+
+select v1 * v2 * v3
+from (
+         select (d1.val + d2.val + d3.val) as sum, d1.val as v1, d2.val as v2, d3.val as v3
+         from day1 d1
+                  cross join day1 d2
+                  cross join day1 d3)
+where sum = 2020;
